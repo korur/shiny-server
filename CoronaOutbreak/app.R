d295761590a6930a975cad711dafa5e6e1bdef28
korur <serdar.korur@gmail.com>
2020-01-26 07:26:01 +0100

    new_Data_coronavirus

--- CoronaOutbreak/app.R
+++ CoronaOutbreak/app.R
@@ -12,18 +12,28 @@
 library(shiny)
 library(shinydashboard)
 library(tidyverse)
-library(plotly)
-library(viridis)
 library(leaflet)
+library(DT)
 
 ASIA <- c("Hong Kong","Japan", "Macau", "Mainland China", "Singapore ", "South Korea", "Taiwan", "Thailand", "Vietnam")
 US <- "US"
 EU <- c("France", "UK", "United Kingdom")
 
+
 # Shiny dashboard App
 data <- readRDS("c_df1.rds")
 data2 <- readRDS("map.rds")
 value <- attr(data,'update')
+df_merge <- readRDS("df_merge.rds")
+
+###################################
+#######                     #######
+#######        HEADER       #######
+#######                     #######
+###################################
+
+
+
 header <- dashboardHeader(
     title= "Coronavirus Cases",
     dropdownMenu(type = "notifications", 
@@ -40,6 +50,12 @@ header <- dashboardHeader(
                          href = "https://www.dataatomic.com"))
 )
 
+###################################
+#######                     #######
+#######        SIDEBAR      #######
+#######                     #######
+###################################
+
 sidebar <- dashboardSidebar(
     
     sidebarMenu(
@@ -54,9 +70,22 @@ sidebar <- dashboardSidebar(
     )
 )
 
+
+###################################
+#######                     #######
+#######          BODY       #######
+#######                     #######
+###################################
+
+
 # combine the two fluid rows to make the body
 body <- dashboardBody( 
     
+################################### 
+#######        TOP ROW      #######
+###################################
+
+
     fluidRow(
                     valueBoxOutput("numcases", width = 2)
                     ,valueBoxOutput("numchina", width = 2)
@@ -65,6 +94,10 @@ body <- dashboardBody(
                     , valueBoxOutput("up", width = 3)
 ), #fluidrow
 
+################################### 
+#######     SECOND ROW      #######
+###################################
+
 fluidRow(
   valueBoxOutput("death", width = 2)
   ,valueBoxOutput("rate", width = 2)
@@ -72,28 +105,73 @@ fluidRow(
   ,valueBoxOutput("continents", width = 2)
   ,valueBoxOutput("update", width = 3)
 ),
+
+
+################################### 
+#######        BOXES 1      #######
+###################################
+
 fluidRow( 
-    box(
-        width = "4"
-        ,solidHeader = TRUE 
-        ,collapsible = TRUE 
-        ,plotlyOutput("countries", width = "500px", height = "700px") 
-        ), #box 
-    box(
-      width = "8"
-      ,solidHeader = TRUE 
-      ,collapsible = TRUE 
-      ,leafletOutput("map", width = "1200px", height = "700px") 
-    )
-       ) #fluidrow
-                    ) #dashboardbody
-               
-               
+  box(
+    width = "6"
+    ,solidHeader = TRUE 
+    ,collapsible = TRUE 
+    ,leafletOutput("map", width = "900px", height = "700px") 
+  ),
+  box(
+    width = "4"
+    ,solidHeader = TRUE 
+    ,collapsible = TRUE 
+    ,plotOutput("casetimeline", width = "700px", height = "700px") 
+  ) #box 
+    
+       ), #fluidrow
 
-#completing the ui part with dashboardPage
+
+################################### 
+#######        BOXES 2      #######
+###################################
+
+fluidRow( 
+  
+  box(
+    width = "6"
+    ,solidHeader = TRUE 
+    ,collapsible = TRUE 
+    ,DT::dataTableOutput("df_wide") 
+    
+  ),
+  box(
+    width = "4"
+    ,solidHeader = TRUE 
+    ,collapsible = TRUE 
+    ,plotOutput("countries", width = "700px", height = "700px") 
+  ) #box 
+  
+),  # fluidrow
+fluidRow( 
+  
+  box(
+    width = "10"
+    ,solidHeader = TRUE 
+    ,collapsible = TRUE 
+    ,plotOutput("facet", width = "1600px", height = "1200px") 
+    
+  ) #box 
+  
+)  # fluidrow
+                    ) # end of dashboardbody
+               
+###################################
+################################### 
+#######                     #######
+#######       SET UI        #######
+#######                     #######
+###################################
+###################################
+
+
 ui <- dashboardPage(title = 'Interactive app - Coronavirus Outbreak', header, sidebar, body, skin= "blue")
-
-
 
 
 
@@ -111,6 +189,13 @@ ui <- dashboardPage(title = 'Interactive app - Coronavirus Outbreak', header, si
 # create the server functions for the dashboard  
 server <- function(input, output) { 
    
+  
+  ###################################
+  #######                     #######
+  #######     VALUE BOXES     #######
+  #######                     #######
+  ###################################
+  
     #creating the valueBoxOutput content
     output$numcases <- renderValueBox({
         valueBox( sum(data$confirmed)
@@ -151,14 +236,14 @@ server <- function(input, output) {
     })
     
     output$death <- renderValueBox({
-      valueBox( print(41)
+      valueBox( print(56)
                
                 , "Total Deaths"
                 ,icon = icon("cross")
                 ,color = "red")  
     })
     output$rate <- renderValueBox({
-      valueBox( round(41/sum(data$confirmed) *100,1)
+      valueBox( round(56/sum(data$confirmed) *100,1)
                
                , "Death rate"
                ,icon = icon('percent')
@@ -182,38 +267,97 @@ server <- function(input, output) {
     })
     output$up <- renderValueBox({
       valueBox( 
-        print("Wuhan Coronavirus Outbreak")
-        , "2019-nCoV"
-        ,icon = icon("virus")
-        ,color = "black") 
+        print("Coronavirus Outbreak")
+        , "Wuhan Coronavirus 2019-nCoV"
+        ,icon = icon("users")
+        ,color = "blue") 
       
     })
-    output$countries <- renderPlotly({
+    
+    ###################################
+    #######                     #######
+    #######     PLOT BOXES 1    #######
+    #######                     #######
+    ###################################
+    
+    output$countries <- renderPlot({
         # Plot
        df_sum <-  data %>% group_by(country) %>% summarise(n=sum(confirmed)) %>% arrange(-n)
          df_sum =df_sum  %>% mutate(country=fct_reorder(country, n, .desc=TRUE))
-        p <- df_sum %>% ggplot(aes(x=country,y=n, fill =n )) + 
+       df_sum %>% ggplot(aes(x=country,y=n, fill =n )) + 
             geom_col() + 
-            scale_fill_viridis( direction =1) + 
             theme_minimal() + 
-            theme(legend.position = "none", axis.text.x=element_text(angle=90),text = element_text(size=12)) +
+            theme(legend.position = "none",text = element_text(size=20), plot.title = element_text( hjust=0.5, vjust = -1)) +
             labs(
-                caption= "   www.dataatomic.com",
+                caption= "www.dataatomic.com",
                 x = "", 
                 y = "Number of cases",
-                title = "Wuhan Coronavirus: \nConfirmed Cases in the World") + 
+                title = "Confirmed Coronavirus Cases per Country") + 
             coord_flip()
-        ggplotly(p)
+        
     })
     
     output$map <- renderLeaflet({
       m <- leaflet() %>%
         addTiles(
           urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
-          attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
-        ) %>% addCircleMarkers(lng=data2$lon, lat=data2$lat, radius = 2* data2$radius, color = "red") %>% 
-        setView(lng = 100, lat = 30, zoom = 3)
+          attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>') %>% 
+        addProviderTiles(providers$Stamen.TonerLite) %>% 
+        addCircleMarkers(lng=data2$lon, lat=data2$lat, radius = 2* data2$radius, color = "red") %>% 
+        setView(lng = 125, lat = 25, zoom = 4)
     })
+    
+    ###################################
+    #######                     #######
+    #######     PLOT BOXES 1    #######
+    #######                     #######
+    ###################################
+    
+    output$casetimeline <- renderPlot({
+      
+      # Plot
+      summary <- df_merge %>% group_by(`Last Update`) %>% summarise(n=sum(Confirmed, na.rm = TRUE))
+      
+      
+      summary %>% ggplot(aes(x=`Last Update`, y=n)) +
+        geom_line(size=2,color='red') + 
+        geom_point(size=8, color='red')+theme_minimal() +
+        theme(legend.position = "none",text = element_text(size=20), plot.title = element_text( hjust=0.5, vjust = -1)) + 
+        labs(
+          caption= "   www.dataatomic.com",
+          x = "Date", 
+          y = "Number of infected people",
+          title = "Global Coronavirus Cases are increasing")
+      
+    }) 
+    
+    output$df_wide <- renderDataTable({
+      
+      dfm <- df_merge[,c(1,3,4)]
+      df_wide <- dfm %>% spread(`Last Update`, Confirmed)
+      df_wide[is.na(df_wide)] <- 0
+   datatable(df_wide, options = list(paging = TRUE), height='400px') 
+      
+    }) 
+    
+    output$facet <- renderPlot({
+      
+      dfm <- df_merge[,c(1,3,4)]
+      
+      
+      dfm %>% mutate(`Province/State`=fct_reorder(`Province/State`,Confirmed, .desc=TRUE))  %>% 
+        ggplot(aes(x=`Last Update`, y=Confirmed))+facet_wrap(.~`Province/State`, scales = "free_y") + 
+        geom_line(color='red') + 
+        geom_point(color='red',size=2) +
+        theme_minimal() + 
+        theme(legend.position="none" , text = element_text(size=20), axis.text.x=element_text(angle=45)) + 
+        labs(
+        caption= "www.dataatomic.com",
+        x = "Time", 
+        y = "Number of cases",
+        title = "Regional increases of Coronavirus Cases with Time")
+      
+    }) 
 }
 shinyApp(ui = ui, server = server)
 
